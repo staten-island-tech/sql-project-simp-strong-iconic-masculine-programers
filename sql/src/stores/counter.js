b86d116688b18ce42e528ac8cec6e4afd98c31a3
snacccc <evanl24@nycstudents.net>
2023-05-30 12:57:19 -0400

    testing pinia

--- sql/src/stores/counter.js
+++ sql/src/stores/counter.js
@@ -1,12 +1,10 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const userTest = defineStore('counter', () => {
+  const username = ref('test')
+  const password = ref('12345')
+  loggedIn = false
 
-  return { count, doubleCount, increment }
+  return { username, password, loggedIn }
 })
