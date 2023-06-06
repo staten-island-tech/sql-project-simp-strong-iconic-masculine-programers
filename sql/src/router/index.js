ae8f9bcaa618f6c93a75f36ef2c3ff7a1793f508
snacccc <evanl24@nycstudents.net>
2023-06-06 13:13:52 -0400

    small framework work

--- sql/src/router/index.js
+++ sql/src/router/index.js
@@ -10,9 +10,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: '/account',
+      name: 'account',
+      component: () => import('../views/Account.vue')
     },
     {
       path: '/',
--- sql/src/views/HomeView.vue
+++ sql/src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
 
+  <div class="createPost">
+    <header>Create a New Post</header>
+    <button>+</button>
+  </div>
 
+  <div class="postWall">
+    <header>Post Wall</header>
+  </div>
 
 </template>
 
