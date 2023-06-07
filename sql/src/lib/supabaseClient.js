03fa6964c9218d4588643170b9f97507af6d882d
BoggyCo <113201377+walter-bIack@users.noreply.github.com>
2023-06-07 03:43:43 -0400

    Fixxed Bug

--- sql/src/lib/supabaseClient.js
+++ sql/src/lib/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://pwvrphowendnbjrujflh.supabase.co/",
-  import.meta.env.VITE_SUPABASE_KEY
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3dnJwaG93ZW5kbmJqcnVqZmxoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwNzEwMzQsImV4cCI6MjAwMTY0NzAzNH0.C3o6UhE-LVdA5eVJiWZ1SbepsQfhSf1amwmvfaoMJUY"
 );
